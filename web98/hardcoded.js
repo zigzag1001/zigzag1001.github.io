11d9d7c8d36e3d1a308a0adb53399a44fa255ce4
zigzag1001 <peter.saburov@gmail.com>
2024-06-29 05:17:13 +0000

    customwindow creation func moved to hardcoded.js

--- web98/hardcoded.js
+++ web98/hardcoded.js
@@ -1,3 +1,4 @@
+// random winow creation controller
 function randomWinodws() {
 	var windowbody = document.createElement('div');
 	windowbody.className = 'window-body';
@@ -49,4 +50,40 @@ function randomWinodws() {
 	div.classList.add('randwincontrol');
 
 	addWindow(div);
+}
+
+
+// custom window creation controller
+function customWin() {
+	var windowbody = document.createElement('div');
+	windowbody.className = 'window-body';
+
+	var form = windowbody.appendChild(document.createElement('div'));
+	form.style.display = 'flex';
+	form.style.flexDirection = 'column';
+	var in_winname = form.appendChild(document.createElement('input'));
+	var in_winbody = form.appendChild(document.createElement('textarea'));
+	var submit = form.appendChild(document.createElement('button'));
+	in_winname.style.padding = '5px';
+	in_winname.placeholder = 'Window Title';
+	in_winbody.placeholder = 'Window Body';
+	in_winbody.style.padding = '5px';
+	submit.textContent = 'Submit';
+	submit.onclick = function() {
+		removeWindow(windowbody.closest('.window'));
+		var div = createWindow({
+			title: in_winname.value,
+			body: simplebody(in_winbody.value)
+		});
+		div.classList.add('customwin');
+		var currentwindow = windowbody.closest('.window');
+		var x = currentwindow.getBoundingClientRect().left + parseInt(currentwindow.style.margin.substring(0, currentwindow.style.margin.length - 2));
+		var y = currentwindow.getBoundingClientRect().top + parseInt(currentwindow.style.margin.substring(0, currentwindow.style.margin.length - 2));
+		addWindow(div, x, y);
+	}
+
+	var custom = createWindow({ body: windowbody, title: 'Create Custom Window' })
+	custom.classList.add('customwincontrol');
+
+	addWindow(custom);
 }
--- web98/script.js
+++ web98/script.js
@@ -129,41 +129,6 @@ function randwin() {
 	}, 1000);
 }
 
-// custom window
-function customWin() {
-	var windowbody = document.createElement('div');
-	windowbody.className = 'window-body';
-
-	var form = windowbody.appendChild(document.createElement('div'));
-	form.style.display = 'flex';
-	form.style.flexDirection = 'column';
-	var in_winname = form.appendChild(document.createElement('input'));
-	var in_winbody = form.appendChild(document.createElement('textarea'));
-	var submit = form.appendChild(document.createElement('button'));
-	in_winname.style.padding = '5px';
-	in_winname.placeholder = 'Window Title';
-	in_winbody.placeholder = 'Window Body';
-	in_winbody.style.padding = '5px';
-	submit.textContent = 'Submit';
-	submit.onclick = function() {
-		removeWindow(windowbody.closest('.window'));
-		var div = createWindow({
-			title: in_winname.value,
-			body: simplebody(in_winbody.value)
-		});
-		div.classList.add('customwin');
-		var currentwindow = windowbody.closest('.window');
-		var x = currentwindow.getBoundingClientRect().left + parseInt(currentwindow.style.margin.substring(0, currentwindow.style.margin.length - 2));
-		var y = currentwindow.getBoundingClientRect().top + parseInt(currentwindow.style.margin.substring(0, currentwindow.style.margin.length - 2));
-		addWindow(div, x, y);
-	}
-
-	var custom = createWindow({ body: windowbody, title: 'Create Custom Window' })
-	custom.classList.add('customwincontrol');
-
-	addWindow(custom);
-}
-
 // Fancy window removal
 function removeWindow(win) {
 	win.classList.add('animate__' + outro);
